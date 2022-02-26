c6b0d2eebba15dd493af9b1946a139a0f99b03d7
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-02-26 15:54:06 +0800

    update orders

--- shop/models.py
+++ shop/models.py
@@ -33,3 +33,19 @@ class Category(models.Model):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     pass
+
+
+class Order(models.Model):
+    # 每一行记录是订单中的一个商品
+    # 同一个订单的order_id相同
+    order_id = models.PositiveIntegerField(null=False)
+    count = models.PositiveIntegerField(null=False)
+    instrument = models.ForeignKey('Instrument', on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=0, null=False)
+    shopper_confirmed = models.BooleanField(default=False)
+    delivery_confirmed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.order_id}<{self.instrument}>'
+
