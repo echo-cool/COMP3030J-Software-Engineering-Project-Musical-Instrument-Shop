1563522b1c27909a787dee9930f4b28737ede3b3
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-02-26 23:23:15 +0800

    order

--- management/views.py
+++ management/views.py
@@ -1,8 +1,8 @@
 import order as order
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.authtoken.admin import User
 
 from app.utils import login_required
 from shop.models import Order, Instrument
@@ -18,11 +18,19 @@ def order_management(request):
     data = []
     orders = Order.objects.all()
     for order_item in orders:
-        tmp = {
-            'order': order_item,
-            'user': User.objects.filter(id=order_item.user.id).first(),
-            'instrument': Instrument.objects(id=order_item.instrument.id).first()
-        }
+        print(order_item.instrument)
+        if order_item.instrument.id is None:
+            tmp = {
+                'order': order_item,
+                'user': User.objects.filter(id=order_item.user.id).first(),
+                'instrument': None
+            }
+        else:
+            tmp = {
+                'order': order_item,
+                'user': User.objects.filter(id=order_item.user.id).first(),
+                'instrument': Instrument.objects.filter(id=order_item.instrument.id).first()
+            }
         data.append(tmp)
     return render(request, 'management_templates/orderManagement.html', {
         'orders': data,
