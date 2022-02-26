190b48d2f7b6fa1ad0cd8e53bbee8798bf6fb4ad
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-02-26 22:57:12 +0800

    login_required

--- management/views.py
+++ management/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from app.utils import login_required
+from shop.models import Order
+
+@login_required
 def index(request):
     return render(request, 'management_templates/index.html')
 
 
+@login_required
 def order_management(request):
-    return render(request, 'management_templates/orderManagement.html')
+    user = request.user
+    orders = Order.objects.filter(user=user.id)
+    return render(request, 'management_templates/orderManagement.html', {
+        'orders': orders,
+    })
