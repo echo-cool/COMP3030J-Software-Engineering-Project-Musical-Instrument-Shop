08792e640c90f2b6de87265ac2e29aa9509a8bfa
MaZixoa <zixiao.ma@ucdconnect.ie>
2022-02-26 16:52:30 +0800

    update

--- management/urls.py
+++ management/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='mangement_index'),
+    path('', views.index, name='management_index'),
+    path('order_management/', views.order_management, name='management_order_management'),
 ]
--- management/views.py
+++ management/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+# The main page of the management page
+from shop.models import Order, Instrument, Category
+
+
 def index(request):
     return render(request, 'index.html')
+
+
+# The order management page
+def order_management(request):
+    orders = Order.objects.order_by("created_at")
+    category = Category.objects.all()
+    print(category)
+    return render(request, 'order_management.html')
