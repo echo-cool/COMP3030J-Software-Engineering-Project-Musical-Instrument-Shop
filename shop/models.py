b144cef34f695dab8ba2a5699af8836fc9e6c813
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-02-26 16:01:33 +0800

    update Instrument table

--- shop/models.py
+++ shop/models.py
@@ -8,6 +8,8 @@ class Instrument(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField(max_length=200)
     details = models.CharField(max_length=3000)
+    image_url = models.FileField(upload_to='uploads/instrument/image/', null=True)
+    object_3d = models.FileField(upload_to='uploads/instrument/obj/', null=True)
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
     category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -49,6 +51,6 @@ class Order(models.Model):
     def __str__(self):
         return f'{self.order_id}<{self.instrument}>'
 
-@admin.register(Category)
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     pass
