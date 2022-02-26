86c67a9d0e3b8efd564fb870aa09123a6fece7ac
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-02-26 23:48:39 +0800

    profile

--- management/views.py
+++ management/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from app.utils import login_required
-from shop.models import Order, Instrument
+from shop.models import Order, Instrument, Profile
 
 
 @login_required
@@ -19,18 +19,13 @@ def order_management(request):
     orders = Order.objects.all()
     for order_item in orders:
         print(order_item.instrument)
-        if order_item.instrument.id is None:
-            tmp = {
-                'order': order_item,
-                'user': User.objects.filter(id=order_item.user.id).first(),
-                'instrument': None
-            }
-        else:
-            tmp = {
-                'order': order_item,
-                'user': User.objects.filter(id=order_item.user.id).first(),
-                'instrument': Instrument.objects.filter(id=order_item.instrument.id).first()
-            }
+
+        tmp = {
+            'order': order_item,
+            'user': User.objects.filter(id=order_item.user.id).first(),
+            'instrument': Instrument.objects.filter(id=order_item.instrument.id).first() if order_item.instrument.id is not None else None,
+            'profile': Profile.objects.filter(id=order_item.user.id).first()
+        }
         data.append(tmp)
     return render(request, 'management_templates/orderManagement.html', {
         'orders': data,
