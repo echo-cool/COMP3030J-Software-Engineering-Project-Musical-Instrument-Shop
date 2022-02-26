c37efbd442cac6b37cecccae96883c01e736937e
MaZixoa <zixiao.ma@ucdconnect.ie>
2022-02-26 22:18:00 +0800

    add order page

--- management/urls.py
+++ management/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'management'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('order_management/', views.order_management, name='order_management')
 ]
--- management/views.py
+++ management/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'management_templates/index.html')
+
+
+def order_management(request):
+    return render(request, 'management_templates/orderManagement.html')
