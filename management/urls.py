21674a8cf818a3ea3aaa5a1d4b96b0727f7c486e
MaZixoa <zixiao.ma@ucdconnect.ie>
2022-02-27 15:39:10 +0800

    add other orders

--- management/urls.py
+++ management/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'management'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('order_management/', views.order_management, name='order_management')
+    path('order_management/all/', views.order_management_all, name='order_management_all'),
+    path('order_management/unconfirmed/', views.order_management_unconfirmed, name='order_management_unconfirmed'),
+    path('order_management/confirmed/', views.order_management_confirmed, name='order_management_confirmed'),
+    path('order_management/delivered/', views.order_management_delivered, name='order_management_delivered')
 ]
--- management/views.py
+++ management/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 
 @login_required
-def order_management(request):
+def order_management_all(request):
     data = []
     orders = Order.objects.all()
     for order_item in orders:
@@ -28,5 +28,69 @@ def order_management(request):
         data.append(tmp)
     return render(request, 'management_templates/orderManagement.html', {
         'orders': data,
-        'profile': Profile.objects.filter(id=request.user.profile.id).first()
+        'profile': Profile.objects.filter(id=request.user.profile.id).first(),
+        'mode': 0
+    })
+
+
+@login_required
+def order_management_unconfirmed(request):
+    data = []
+    orders = Order.objects.filter(shopper_confirmed=False)
+    for order_item in orders:
+        print(order_item.instrument)
+
+        tmp = {
+            'order': order_item,
+            'user': User.objects.filter(id=order_item.user.id).first(),
+            'instrument': Instrument.objects.filter(id=order_item.instrument.id).first() if order_item.instrument.id is not None else None,
+            'profile': Profile.objects.filter(id=order_item.user.profile.id).first() if order_item.user.profile.id is not None else None
+        }
+        data.append(tmp)
+    return render(request, 'management_templates/orderManagement.html', {
+        'orders': data,
+        'profile': Profile.objects.filter(id=request.user.profile.id).first(),
+        'mode': 1
+    })
+
+
+@login_required
+def order_management_confirmed(request):
+    data = []
+    orders = Order.objects.filter(shopper_confirmed=True).filter(delivery_confirmed=False)
+    for order_item in orders:
+        print(order_item.instrument)
+
+        tmp = {
+            'order': order_item,
+            'user': User.objects.filter(id=order_item.user.id).first(),
+            'instrument': Instrument.objects.filter(id=order_item.instrument.id).first() if order_item.instrument.id is not None else None,
+            'profile': Profile.objects.filter(id=order_item.user.profile.id).first() if order_item.user.profile.id is not None else None
+        }
+        data.append(tmp)
+    return render(request, 'management_templates/orderManagement.html', {
+        'orders': data,
+        'profile': Profile.objects.filter(id=request.user.profile.id).first(),
+        'mode': 2
+    })
+
+
+@login_required
+def order_management_delivered(request):
+    data = []
+    orders = Order.objects.filter(delivery_confirmed=True)
+    for order_item in orders:
+        print(order_item.instrument)
+
+        tmp = {
+            'order': order_item,
+            'user': User.objects.filter(id=order_item.user.id).first(),
+            'instrument': Instrument.objects.filter(id=order_item.instrument.id).first() if order_item.instrument.id is not None else None,
+            'profile': Profile.objects.filter(id=order_item.user.profile.id).first() if order_item.user.profile.id is not None else None
+        }
+        data.append(tmp)
+    return render(request, 'management_templates/orderManagement.html', {
+        'orders': data,
+        'profile': Profile.objects.filter(id=request.user.profile.id).first(),
+        'mode': 3
     })
