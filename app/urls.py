2c3a11e4dbfedea9e692a653665bee0f2866058a
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-03-05 00:15:41 +0800

    bug-fix

--- app/urls.py
+++ app/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', IndexPageView.as_view(), name='index'),
     path('', include('shop.urls'), name="index"),
-    path('model/', include('shop.urls'), name="model"),
     path('management/', include('management.urls'), name='management'),
 
     path('i18n/', include('django.conf.urls.i18n')),
--- shop/urls.py
+++ shop/urls.py
@@ -5,8 +5,8 @@ from .views import checkout, confirm, model, index, cart
 app_name = 'shop'
 urlpatterns = [
     path('', index, name='index'),
-    path('checkout', checkout, name='checkout'),
-    path('confirm', confirm, name='confirm'),
-    path("model", model, name='model'),
-    path('cart', cart, name='cart'),
+    path('checkout/', checkout, name='checkout'),
+    path('confirm/', confirm, name='confirm'),
+    path("model/", model, name='model'),
+    path('cart/', cart, name='cart'),
 ]
