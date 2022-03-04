cd798fedef88e238ebc2a250af9ca1f48cda6e68
Zheyu Chen <zheyu.chen@ucdconnect.ie>
2022-03-04 17:34:39 +0800

    create_order.html init

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+from .views import create_order;
+
 app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_order', create_order, name='create_order'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'shop_templates/index.html')
+
+
+def create_order(request):
+    return render(request, 'shop_templates/create_order.html')
