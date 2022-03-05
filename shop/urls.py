94acead2eebe188357831e1f57928818b9fea087
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-03-05 12:00:18 +0800

    product_details

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,8 @@ from .views import checkout, confirm, model, index, cart, product_details
 
 app_name = 'shop'
 urlpatterns = [
-    path('', product_details, name='product_details'),
+    path('', index, name='index'),
+    path("product_details/<int:product_id>", product_details, name="product_details"),
     path('checkout/', checkout, name='checkout'),
     path('confirm/', confirm, name='confirm'),
     path("model/", model, name='model'),
