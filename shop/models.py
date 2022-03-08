d5cdea61daa3b17263923acba9d04af1c5ece363
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-03-08 12:30:24 +0800

    change-database-config

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Activation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    code = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(blank=True)
 
 # Create your models here.
 class Profile(models.Model):
