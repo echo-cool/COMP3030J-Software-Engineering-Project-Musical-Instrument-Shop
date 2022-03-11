d568bca5b168bfc98cb6929a162992e8813091d2
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-03-11 16:26:19 +0800

    blog

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,30 @@ from blog.models import Post
 
 def index(request):
     posts = Post.objects.all()
+    if len(posts) >= 3:
+        latest3Posts = posts[:3]
+    elif len(posts) == 2:
+        latest3Posts = posts[:2]
+    else:
+        latest3Posts = posts[:1]
+
     return render(request, 'blog_templates/index.html', {
         'posts': posts,
+        'latest3Posts': latest3Posts,
     })
 
 
 def view(request, post_id):
     post = get_object_or_404(Post, id=post_id)
+    posts = Post.objects.all()
+    if len(posts) >= 3:
+        latest3Posts = posts[:3]
+    elif len(posts) == 2:
+        latest3Posts = posts[:2]
+    else:
+        latest3Posts = posts[:1]
+
     return render(request, 'blog_templates/view.html', {
         'post': post,
+        'latest3Posts': latest3Posts,
     })
