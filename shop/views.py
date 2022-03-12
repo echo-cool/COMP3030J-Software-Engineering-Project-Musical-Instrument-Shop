b1756c8520c5bc527cb95106a8d356ad73377d0e
wenghan1 <han.weng@ucdconnect.ie>
2022-03-12 15:52:11 +0800

    merge conflict

--- shop/views.py
+++ shop/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 
 from management.forms import InstrumentForm, SearchForm
-from shop.models import Instrument, InstrumentDetail, Category, Order, Review, Item
+from shop.models import Instrument, InstrumentDetail, Category, Order, Review
 
 
 def index(request):
@@ -15,13 +15,23 @@ def index(request):
     categories = Category.objects.all()
     for i in instruments:
         i.percentage = round(i.price * 100 / i.old_price, 2)
-    return render(request, 'shop_templates/index2.html', {
+    return render(request, 'shop_templates/index.html', {
         "instruments": instruments,
         "categories": categories
     })
 
+def category_view(request, category_id):
+    categories = Category.objects.all()
+    category = get_object_or_404(Category, pk=category_id)
+    instruments = Instrument.objects.filter(category=category)
+    return render(request, 'shop_templates/category/category_view.html', {
+        "category": category,
+        "instruments": instruments,
+        "categories": categories
+    })
 
 def product_details(request, product_id):
+    categories = Category.objects.all()
     instrument = Instrument.objects.get(id=product_id)
     instrument_details = InstrumentDetail.objects.filter(instrument=instrument).first()
     all_instruments = Instrument.objects.all()
@@ -33,7 +43,8 @@ def product_details(request, product_id):
         "instrument": instrument,
         "discount": instrument.price * 100 / instrument.old_price,
         "instrument_details": instrument_details,
-        "related": related
+        "related": related,
+        'categories': categories
     })
 
 
@@ -44,23 +55,21 @@ def leave_review(request, order_id, instrument_id):
 def confirm_submit(request):
     if request.method == "POST":
         rating = request.POST.get("rating-input", None)
-        title = request.POST.get("title", None)
         review_text = request.POST.get("review", None)
         fileupload = request.POST.get("fileupload", None)
-        check_selected = request.POST.get("check", None)
+        # check_selected = request.POST.get("check", None)
         print("rating: ", rating)
-        print("title: ", title)
         print("review_text: ", review_text)
         print("fileupload: ", fileupload)
-        print("check_selected: ", check_selected)
+        # print("check_selected: ", check_selected)
         # f = ReviewForm(request.POST, request.FILES)
         # if f.is_valid():
         #     f.save()
         # print(f.errors)
-        new_review = Review(order_id=1, user_id=1, rating=rating, title=title,
-                            review_text=review_text, fileupload=fileupload, check_selected=check_selected)
+        new_review = Review(order_id=1, user_id=1, rating=rating,
+                            review_text=review_text, fileupload=fileupload)
         new_review.save()
-    return render(request, 'shop_templates/leave-review-2.html')
+    return render(request, 'shop_templates/product-detail-2.html')
 
 
 def leave_review2(request):
@@ -74,27 +83,27 @@ def model_view(request, product_id):
         "instrument": instrument,
     })
 
-
-def checkout(request):
-    new_item = Item(item_id=0)
-    new_item.save()
-    return render(request, 'shop_templates/checkout.html', {
-        "id": new_item.id,
-    })
-
-
-def confirm(request):
-    new_order = Order(user=request.user, name=request.POST['name'], last_name=request.POST['last_name'],
-                      full_address=request.POST['full_address'], city=request.POST['city'],
-                      postal_code=request.POST['postal_code'], country=request.POST['country'],
-                      telephone=request.POST['telephone'], payment=request.POST['payment'],
-                      shipping=request.POST['shipping'])
-    new_order.save()
-    # b_row = Item.objects.get(id=request.POST['item_id'])
-    # b_row.Order = new_order
-    # b_row.save()
-    Item.objects.filter(id=request.POST['item_id']).update(Order=new_order)
-    return render(request, 'shop_templates/confirm.html')
+#
+# def checkout(request):
+#     new_item = Item(item_id=0)
+#     new_item.save()
+#     return render(request, 'shop_templates/checkout.html', {
+#         "id": new_item.id,
+#     })
+#
+#
+# def confirm(request):
+#     new_order = Order(user=request.user, name=request.POST['name'], last_name=request.POST['last_name'],
+#                       full_address=request.POST['full_address'], city=request.POST['city'],
+#                       postal_code=request.POST['postal_code'], country=request.POST['country'],
+#                       telephone=request.POST['telephone'], payment=request.POST['payment'],
+#                       shipping=request.POST['shipping'])
+#     new_order.save()
+#     # b_row = Item.objects.get(id=request.POST['item_id'])
+#     # b_row.Order = new_order
+#     # b_row.save()
+#     Item.objects.filter(id=request.POST['item_id']).update(Order=new_order)
+#     return render(request, 'shop_templates/confirm.html')
 
 
 def model_design(request):
