51699b3a0e7d946bd68b98df07787c44ffc40e41
Yang Liuxin <liuxin.yang@ucdconnect.ie>
2022-03-12 15:39:18 +0800

    delete

--- shop/models.py
+++ shop/models.py
@@ -105,7 +105,9 @@ class Order(models.Model):
     telephone = models.CharField(max_length=200, default="(000)000-0000")
     payment = models.CharField(max_length=20, default="")
     shipping = models.CharField(max_length=20, default="")
-
+    instrument = models.ForeignKey('Instrument', on_delete=models.CASCADE, default=0)
+    count = models.PositiveIntegerField(null=False, default=1)
+    # Item = models.ForeignKey('Item', max_length=200, on_delete=models.CASCADE, null=True)
     newsletter = models.BooleanField(default=False)
     shopper_confirmed = models.BooleanField(default=False)
     delivery_confirmed = models.BooleanField(default=False)
@@ -119,34 +121,7 @@ class Order(models.Model):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = (
-        'user', "order_id", 'name', 'last_name', 'full_address', 'city', 'postal_code', 'country', 'telephone', 'payment',
-        'shipping', 'newsletter', 'shopper_confirmed', 'delivery_confirmed', 'created_at')
-
-
-class Item(models.Model):
-    item_id = models.CharField(max_length=200, default="")
-    Order = models.ForeignKey('Order', on_delete=models.CASCADE, null=True)
-    instrument = models.ForeignKey('Instrument', on_delete=models.CASCADE, null=True)
-    count = models.PositiveIntegerField(null=False, default=1)
-
-
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('item_id', 'instrument', 'count', 'Order')
-
-
-# class OrderItem(models.Model):
-#     order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-#     count = models.PositiveIntegerField(null=False)
-#     instrument = models.ForeignKey('Instrument', on_delete=models.CASCADE, null=False)
-#
-#     def __str__(self):
-#         return f'{self.order_id}<{self.instrument}:{self.count}>'
-#
-#
-# @admin.register(OrderItem)
-# class OrderItemAdmin(admin.ModelAdmin):
-#     pass
+        'user', "order_id", 'name', 'last_name', 'shopper_confirmed', 'delivery_confirmed', 'created_at')
 
 
 class Review(models.Model):
@@ -157,13 +132,13 @@ class Review(models.Model):
         default=5,
         validators=[MaxValueValidator(5), MinValueValidator(0)]
     )
-    title = models.CharField(null=True, max_length=30)
+    # title = models.CharField(null=True, max_length=30)
     review_text = models.TextField(null=True)
-    fileupload = models.ImageField(default='default.jpg', upload_to='uploads/avatar/image/')
+    file_upload = models.ImageField(default='default.jpg', upload_to='uploads/review/image/')
     check_selected = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('order_id', 'user', 'rating', 'title', 'review_text', 'fileupload', 'check', 'created_at')
+    list_display = ('order_id', 'user', 'rating', 'review_text', 'file_upload', 'check', 'created_at')
