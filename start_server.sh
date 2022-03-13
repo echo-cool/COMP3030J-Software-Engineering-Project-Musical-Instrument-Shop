cc23cd86d94845a9a97c6157fbc85ebfce30c606
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-03-13 17:39:44 +0800

    update-start-sh

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,8 @@
-python3 manage.py makemigrations
-python3 manage.py migrate
+python -m pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py makemigrations shop
+python manage.py migrate shop
+
 
 python3 manage.py runserver 0.0.0.0:8881
