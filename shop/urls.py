09fba8e79fae23d3825b2662a5f462cad3db5b62
Zhan Yi <yi.zhan@ucdconnect.ie>
2022-03-14 17:53:00 +0800

    new product detail page with model

--- shop/urls.py
+++ shop/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from .views import model_view, index, cart, product_details, model_design, leave_review, \
     leave_review2, \
     confirm_submit, product_search, product_search_by_category, empty_search, category_view, product_add_cart, \
-    product_minus_cart
+    product_minus_cart, product_details_test_model
 
 app_name = 'shop'
 urlpatterns = [
     path('', index, name='index'),
     path("product_details/<int:product_id>", product_details, name="product_details"),
+    path("product_details_test_model/<int:product_id>", product_details_test_model,
+         name="product_details_test_model"),
     path("leave_review/<int:order_id>/<int:instrument_id>", leave_review, name="leave_review"),
     # path("leave_review2/", leave_review2, name="leave_review2"),
     path("confirm_submit/", confirm_submit, name="confirm_submit"),
--- shop/views.py
+++ shop/views.py
@@ -101,7 +101,7 @@ def leave_review2(request):
 
 def model_view(request, product_id):
     instrument = get_object_or_404(Instrument, pk=product_id)
-    return render(request, 'shop_templates/3d3.html', {
+    return render(request, 'shop_templates/product-detail-model.html', {
         "instrument": instrument,
     })
 
@@ -232,3 +232,21 @@ def product_minus_cart(request, instrument_id):
         exist_cart.delete()
         exist_cart.save()
     return redirect('shop:cart')
+
+
+def product_details_test_model(request, product_id):
+    categories = Category.objects.all()
+    instrument = Instrument.objects.get(id=product_id)
+    instrument_details = InstrumentDetail.objects.filter(instrument=instrument).first()
+    all_instruments = Instrument.objects.all()
+    related = []
+    for i in range(5):
+        num = random.randint(0, len(all_instruments) - 1)
+        related.append(all_instruments[num])
+    return render(request, 'shop_templates/product-detail-2.html', {
+        "instrument": instrument,
+        "discount": instrument.price * 100 / instrument.old_price,
+        "instrument_details": instrument_details,
+        "related": related,
+        'categories': categories
+    })
