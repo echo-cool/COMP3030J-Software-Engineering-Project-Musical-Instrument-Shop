2c184dda6045b6b43a4543d030f63a5a5479f27d
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-03-15 17:10:40 +0800

    update_deployment

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,8 @@
 python3 -m pip install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py makemigrations shop
-python3 manage.py migrate shop
+#python3 manage.py makemigrations
+#python3 manage.py migrate
+#python3 manage.py makemigrations shop
+#python3 manage.py migrate shop
 
 
 python3 manage.py runserver 0.0.0.0:8881
