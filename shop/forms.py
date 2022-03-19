807a4fbbe36dd595aa3d0fbe99d012e78cf86bf2
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-03-19 15:59:29 +0800

    update-shop-app-form.pt

---
+++ shop/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Time : 3/19/2022 3:57 PM
+@Auth : Wang Yuyang
+@File : forms.py
+@IDE  : PyCharm
+"""
+from django import forms
+from django.forms import ValidationError
+
+class UpdateProfileForm(forms.Form):
