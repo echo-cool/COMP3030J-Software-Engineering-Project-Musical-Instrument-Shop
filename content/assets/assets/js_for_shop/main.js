0b88b5f32430892133045ff658fc5d692812eacd
wenghan1 <han.weng@ucdconnect.ie>
2022-03-23 17:43:40 +0800

    model page backup

--- content/assets/assets/js_for_shop/main.js
+++ content/assets/assets/js_for_shop/main.js
@@ -1,22 +1,3 @@
-function GetUrlParam(paraName) {
-    var url = document.location.toString();
-    var arrObj = url.split("?");
-    if (arrObj.length > 1) {
-        var arrPara = arrObj[1].split("&");
-        var arr;
-        for (var i = 0; i < arrPara.length; i++) {
-            arr = arrPara[i].split("=");
-            if (arr != null && arr[0] === paraName) {
-                return arr[1];
-            }
-        }
-        return "";
-    } else {
-        return "";
-    }
-}
-
-
 function fetchHeaderCartList() {
     console.log("fetched_carts");
     let fetched_carts = {};
