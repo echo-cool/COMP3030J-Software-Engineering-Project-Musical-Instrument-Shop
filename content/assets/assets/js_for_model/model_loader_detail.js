9553bd46bc57a811585a8984b176402cb736f2f8
Zhan Yi <yi.zhan@ucdconnect.ie>
2022-03-23 17:17:59 +0800

    new js files

--- content/assets/assets/js_for_model/model_loader_detail.js
+++ content/assets/assets/js_for_model/model_loader_detail.js
@@ -152,24 +152,6 @@ function animate() {
     }
 }
 
-// 获取参数
-function GetUrlParam(paraName) {
-    var url = document.location.toString();
-    var arrObj = url.split("?");
-    if (arrObj.length > 1) {
-        var arrPara = arrObj[1].split("&");
-        var arr;
-        for (var i = 0; i < arrPara.length; i++) {
-            arr = arrPara[i].split("=");
-            if (arr != null && arr[0] === paraName) {
-                return arr[1];
-            }
-        }
-        return "";
-    } else {
-        return "";
-    }
-}
 
 let fake = 0;
 
