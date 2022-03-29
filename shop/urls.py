62580820bbdce6a3bd6beab3882763a841f5b36a
yangliuxin <liuxin.yang@ucdconnect.ie>
2022-03-29 17:02:10 +0800

    company profile

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from .views import model_view, index, cart, product_details, model_design, leave
     product_details_test_model, wishlist, personal_profile, \
     checkout, confirm, model_design2, home, \
     orders, confirm_submit, product_search, product_search_by_category, category_view, personal_profile, \
-    product_add_cart
+    product_add_cart, about
 
 app_name = 'shop'
 urlpatterns = [
@@ -30,4 +30,5 @@ urlpatterns = [
     path('wishlist/', wishlist, name='wishlist'),
     path('orders/<int:user_id>', orders, name='orders'),
     path('home', home, name='home'),
+    path('about', about, name='about'),
 ]
