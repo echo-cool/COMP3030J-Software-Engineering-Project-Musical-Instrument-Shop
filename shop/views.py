7e3847f0dd7705d1ddfb5560e069e2da877b90ce
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-04-08 23:09:57 +0800

    review

--- shop/views.py
+++ shop/views.py
@@ -94,7 +94,14 @@ def product_details(request, product_id):
 
     related = []
     # Get 4 random reviews
-    review = Review.objects.filter(instrument=instrument).order_by('?')[:4]
+    reviews = Review.objects.all()
+    review = []
+    for i in range(4):
+        num = random.randint(0, len(reviews) - 1)
+        reviews[num].review_icon_iter = range(int(reviews[num].rating))
+        reviews[num].review_icon_iter2 = range(5 - int(reviews[num].rating))
+        review.append(reviews[num])
+
     for i in range(5):
         num = random.randint(0, len(all_instruments) - 1)
         related.append(all_instruments[num])
@@ -105,7 +112,8 @@ def product_details(request, product_id):
         "related": related,
         'categories': categories,
         "review_left": [review[0], review[1]],
-        "review_right": [review[2], review[3]]
+        "review_right": [review[2], review[3]],
+
     })
 
 
