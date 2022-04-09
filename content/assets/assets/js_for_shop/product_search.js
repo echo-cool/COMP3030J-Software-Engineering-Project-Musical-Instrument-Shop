c7d08a26abd379758e62f247b60c46f525f96623
wenghan1 <han.weng@ucdconnect.ie>
2022-04-09 18:53:58 +0800

    product search improve

--- content/assets/assets/js_for_shop/product_search.js
+++ content/assets/assets/js_for_shop/product_search.js
@@ -1,4 +1,5 @@
 js_draw_filter();
+activateCol();
 
 function js_draw_filter() {
     console.log("activate filter");
@@ -18,6 +19,71 @@ function js_draw_filter() {
         })
         setTimeout(function () {
             js_draw_filter();
-        })
-    })
+        });
+    });
 }
+
+function activateCol() {
+    console.log("activate  COL");
+    let arrays = $(".prod_per>a");
+    arrays.click(function () {
+        for (let i of arrays) {
+            i.className = i.className.replace("active", "");
+        }
+        console.log(this.getAttribute("class"));
+        this.className = this.className + " active";
+        removeCol();
+        if (this.className.includes("size_2")) {
+            $(".js_size_prod").addClass('col-lg-6');
+        } else if (this.className.includes("size_3")) {
+            $(".js_size_prod").addClass('col-lg-4');
+        } else if (this.className.includes("size_4")) {
+            $(".js_size_prod").addClass('col-lg-3');
+        } else if (this.className.includes("size_5")) {
+            $(".js_size_prod").addClass('col-lg-2');
+        }
+    })
+
+
+    function removeCol() {
+        $(".js_size_prod").removeClass('col-lg-6').removeClass('col-lg-4').removeClass('col-lg-3').removeClass('col-lg-2');
+    }
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+// jQuery(document).ready(function ($) {
+//     setInterval(function () {
+//         $('.popup_random_prod').addClass('hide_popup_random_prod');
+//     }, 10000)
+//     $('.close_popup').on('click', function () {
+//         $('.popup_random_prod').addClass('hide_popup_random_prod');
+//
+//     });
+//     setInterval(function () {
+//         $('.popup_random_prod').removeClass('hide_popup_random_prod');
+//         var dem = 8 - 1;
+//         var initial2 = random(0, dem);
+//
+//         var count2 = initial2;
+//         $('.popup_random_prod').find('ul').children().addClass('d-none');
+//         $('.popup_random_prod').find('ul').children().eq(count2).removeClass('d-none');
+//         var initial = random(1, 60);
+//         var count = initial;
+//         var x = document.getElementsByClassName("random_minute");
+//         x[count2].innerHTML = count;
+//
+//         var max = $('.js-readname').attr('data-max') - 1;
+//         var text = $('.js-readname').children().eq(random(0, max)).text();
+//         $('.js-name').text(text);
+//
+//         var max2 = $('.js-readaddress').attr('data-max') - 1;
+//         var text2 = $('.js-readaddress').children().eq(random(0, max2)).text();
+//         $('.js-address').text(text2);
+//
+//     }, 15000)
+// })
+
