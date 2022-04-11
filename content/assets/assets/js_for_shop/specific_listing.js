9667a1a03b160c702eb1931d882a4051c1eabc20
MaZixoa <zixiao.ma@ucdconnect.ie>
2022-04-11 14:04:04 +0800

    category

--- content/assets/assets/js_for_shop/specific_listing.js
+++ content/assets/assets/js_for_shop/specific_listing.js
@@ -89,18 +89,31 @@ function activateCategorySearch() {
         // })
 
         categories.on("click", function () {
-            var param = "";
 
+            var param = "";
+            let that = this;
+            // $(this).prev().attr("checked", !$(this).prev().is(':checked'));\
+            if ($(this).prev().is(':checked')) {
+                $(this).prev().removeAttr("checked");
+            }
+            else {
+                $(this).prev().attr("checked", 'checked');
+            }
+            let categories = $(".category-name");
             categories.each(function () {
                 var checkbox = $(this).prev();
-                console.log(checkbox);
+                console.log(checkbox.is(':checked'));
                 if (checkbox.is(':checked')) {
                     param += "|" + this.id.split('__')[1];
                 }
             });
             param = param.substring(1);
             console.log(param)
-            console.log(addUrlPara(window.location.href, 'category', param))
+            console.log(categories)
+            categories.each(function () {
+                console.log($(this).prev())
+            })
+
             // window.location.href = addUrlPara(window.location.href, 'category', param);
         })
 
@@ -108,8 +121,19 @@ function activateCategorySearch() {
             const checked_list = getQueryString('category').split('|');
             if (checked_list.length > 0) {
                 $checkboxes.each(function () {
-                    this.checked = checked_list.includes(this.id.split('__')[1]);
-                    $(this).next().addClass('active')
+                    // this.checked = checked_list.includes(this.id.split('__')[1]);
+                    if (checked_list.includes(this.id.split('__')[1])) {
+                        $(this).attr("checked", true);
+                        if (!$(this).next().hasClass('active')) {
+                            $(this).next().addClass('active')
+                        }
+                    }
+                    else {
+                        $(this).attr("checked", false);
+                        if ($(this).next().hasClass('active')) {
+                            $(this).next().removeClass('active')
+                        }
+                    }
                 });
             }
         }
