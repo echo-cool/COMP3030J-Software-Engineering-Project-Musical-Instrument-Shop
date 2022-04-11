12c75b5b542cdcaff7e0c2eeca5de5981560ed3f
wenghan1 <han.weng@ucdconnect.ie>
2022-04-11 15:26:28 +0800

    backend template

--- management/urls.py
+++ management/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'management'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new', views.new, name='new'),
+    path('order_management/all/new', views.order_management_all_new, name='order_management_all_new'),
+    path('instrument_management/new', views.instrument_management_new, name='instrument_management_new'),
     path('order_management/all/', views.order_management_all, name='order_management_all'),
     path('order_management/unconfirmed/', views.order_management_unconfirmed, name='order_management_unconfirmed'),
     path('order_management/confirmed/', views.order_management_confirmed, name='order_management_confirmed'),
