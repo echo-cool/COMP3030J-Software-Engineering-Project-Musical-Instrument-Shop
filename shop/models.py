43758f3976db95716540968c91f0c17e75256a75
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-04-15 12:30:18 +0800

    order checkout Include db change and template change

--- shop/models.py
+++ shop/models.py
@@ -115,21 +115,45 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'created_at', 'main_image')
 
 
+class UncompletedOrder(models.Model):
+    # 这个是用来表示用户输入了地址但是还没有付款的订单，防止订单计算错误
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    first_name = models.CharField(max_length=20, default="", null=True)
+    last_name = models.CharField(max_length=20, default="")
+    address = models.CharField(max_length=200, default="")
+    apartment = models.CharField(max_length=200, default="")
+    city = models.CharField(max_length=20, default="")
+    country = models.CharField(max_length=200, default="")
+    zip_Code = models.CharField(max_length=50, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}<{self.created_at}>'
+
+
+@admin.register(UncompletedOrder)
+class UnCompletedOrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'first_name', 'last_name', 'address', 'apartment', 'city', 'country', 'zip_Code', 'created_at')
+
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
     # This is used to solve the problem of one order has more than one instrument
     order_id = models.IntegerField(default=0)
 
-    name = models.CharField(max_length=20, default="", null=True)
+    first_name = models.CharField(max_length=20, default="", null=True)
     last_name = models.CharField(max_length=20, default="")
-    full_address = models.CharField(max_length=200, default="")
+    address = models.CharField(max_length=200, default="")
+    apartment = models.CharField(max_length=200, default="")
     city = models.CharField(max_length=20, default="")
-    postal_code = models.CharField(max_length=50, default="")
     country = models.CharField(max_length=200, default="")
-    telephone = models.CharField(max_length=200, default="(000)000-0000")
-    payment = models.CharField(max_length=20, default="")
-    shipping = models.CharField(max_length=20, default="")
+    zip_Code = models.CharField(max_length=50, default="")
+
+    # telephone = models.CharField(max_length=200, default="(000)000-0000")
+    # payment = models.CharField(max_length=20, default="")
+    # shipping = models.CharField(max_length=20, default="")
     instrument = models.ForeignKey('Instrument', on_delete=models.CASCADE, null=True)
     quantity = models.PositiveIntegerField(null=False, default=1)
     subtotal = models.FloatField(null=False, default=0)
@@ -149,9 +173,7 @@ class Order(models.Model):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = (
-        'user', "order_id", 'name', 'last_name', 'full_address', 'city', 'postal_code', 'country', 'telephone',
-        'payment', 'subtotal',
-        'shipping', 'instrument', 'quantity', 'newsletter', 'accepted', 'packed', 'shipped', 'delivered', 'created_at')
+        'user', 'first_name', 'last_name', 'address', 'apartment', 'city', 'country', 'zip_Code', 'subtotal', 'instrument', 'quantity', 'newsletter', 'accepted', 'packed', 'shipped', 'delivered', 'created_at')
 
 
 # class OrderItem(models.Model):
