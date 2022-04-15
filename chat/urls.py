9e6c3de68ca53ff33e8b6e7ce2d397c6ef97957d
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-04-15 22:36:25 +0800

    resa

--- chat/urls.py
+++ chat/urls.py
@@ -8,11 +8,12 @@
 
 from django.urls import path
 
-from chat.views import index, index_new, rasa_chat
+from chat.views import index, index_new, rasa_chat, ai_chat_test
 
 app_name = 'chat'
 urlpatterns = [
     path('', index, name='index'),
     path('new/<int:to>', index_new, name="index_new"),
-    path("rasa/<str:message>", rasa_chat, name="rasa"),
+    path("rasa/", rasa_chat, name="rasa"),
+    path("ai_chat_test/",ai_chat_test, name="ai_chat_test"),
 ]
