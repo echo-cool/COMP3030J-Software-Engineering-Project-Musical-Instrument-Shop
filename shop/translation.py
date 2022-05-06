5e9f5aa2eed947378622b4e53e1742f1d563029f
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2022-05-06 20:57:54 +0800

    model-translatio

--- shop/translation.py
+++ shop/translation.py
@@ -1,9 +1,16 @@
+from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 from modeltranslation.translator import translator, TranslationOptions
 from .models import Instrument
 
 
-class InstrumentTranslationOptions(TranslationOptions):
-    fields = ('name', 'details', 'ex_detail', 'ad_info')
-
-
-translator.register(Instrument, InstrumentTranslationOptions)
+# class InstrumentTranslationOptions(TranslationOptions):
+#     fallback_languages = {'default': ('en',)}
+#     fields = ('name', 'details', 'ex_detail', 'ad_info')
+#
+# class InstrumentAdmin(TranslationAdmin):
+#     list_display = ('name', 'price', 'image', 'object_3d', 'object_mtl', 'posted_by', 'category', 'created_at')
+#
+#
+# translator.register(Instrument, InstrumentTranslationOptions)
+# admin.site.register(Instrument, InstrumentAdmin)
